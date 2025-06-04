35f9d5a444752a0132a7727ae215daffb0585da2
Mr Jestor <108587930+wijesuriya2017@users.noreply.github.com>
2025-06-05 00:32:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'MR NAVI‎',
   botName: "Knight Bot",
   botOwner: 'RC JESTOR', // Your name
-  ownerNumber: '94702264309', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '94710592882', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
