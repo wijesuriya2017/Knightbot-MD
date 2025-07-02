caf834db1de9cede7715cc7f06f36e75d6281c38
Mr Jestor <108587930+wijesuriya2017@users.noreply.github.com>
2025-07-02 17:39:47 +0530

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -37,7 +37,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '120363421047540687@newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
@@ -52,7 +52,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '120363421047540687@newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
