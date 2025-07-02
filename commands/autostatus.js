a0e9ee54ac81c45aaeb3af2affb7a55e55832c78
Mr Jestor <108587930+wijesuriya2017@users.noreply.github.com>
2025-07-02 17:29:41 +0530

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,7 +6,7 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
+            newsletterJid: '120363421047540687@newsletter',
             newsletterName: 'KnightBot MD',
             serverMessageId: -1
         }
