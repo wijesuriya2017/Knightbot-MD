38b6d06c26aa51582f90059051e99ff39a24bc3a
Mr Jestor <108587930+wijesuriya2017@users.noreply.github.com>
2025-07-29 15:24:20 +0530

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,7 +65,7 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '120363421047540687@newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
