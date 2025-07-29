6f337abb49204f8bafb629f8c21978ef3551d9d9
Mr Jestor <108587930+wijesuriya2017@users.noreply.github.com>
2025-07-29 15:31:30 +0530

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -36,7 +36,7 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '120363421047540687@newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
@@ -58,7 +58,7 @@ async function stickerCommand(sock, chatId, message) {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
+                        newsletterJid: '120363421047540687@newsletter',
                         newsletterName: 'KnightBot MD',
                         serverMessageId: -1
                     }
@@ -146,7 +146,7 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '120363421047540687@newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
